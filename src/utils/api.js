3398f8511e0980b8f4b06b4348bcda303be87dbe
DesireeJoy <desireebradish@gmail.com>
2021-05-10 10:55:45 -0400

    Remove catches in api server calls

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,9 +10,7 @@ class Api {
   getInitialCards() {
     return fetch(this._baseUrl + "/cards", {
       headers: this._headers,
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
+    }).then(this._checkResponse);
   }
   _checkResponse(res) {
     return res.ok ? res.json() : Promise.reject("Error!" + res.statusText);
@@ -22,9 +20,7 @@ class Api {
   getUserInfo() {
     return fetch(this._baseUrl + "/users/me", {
       headers: this._headers,
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
+    }).then(this._checkResponse);
   }
   getAllInfo() {
     return Promise.all([this.getUserInfo(), this.getInitialCards()]);
@@ -39,9 +35,7 @@ class Api {
         name,
         about,
       }),
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
+    }).then(this._checkResponse);
   }
   //POST https://around.nomoreparties.co/v1/groupId/cards
   addCard({ name, link }) {
@@ -52,18 +46,14 @@ class Api {
         name,
         link,
       }),
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
+    }).then(this._checkResponse);
   }
   removeCard(cardId) {
     //fetch cards + cardid
     return fetch(this._baseUrl + "/cards/" + cardId, {
       method: "DELETE",
       headers: this._headers,
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
+    }).then(this._checkResponse);
   }
   //PATCH https://around.nomoreparties.co/v1/groupId/users/me/avatar
   setAvatar({ avatar }) {
@@ -73,9 +63,7 @@ class Api {
       body: JSON.stringify({
         avatar,
       }),
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log("Error! " + err));
+    }).then(this._checkResponse);
   }
   // PUT https://around.nomoreparties.co/v1/groupId/cards/likes/cardId
   // DELETE https://around.nomoreparties.co/v1/groupId/cards/likes/cardId
@@ -85,9 +73,7 @@ class Api {
     return fetch(this._baseUrl + "/cards/likes/" + cardId, {
       method: whichMethod,
       headers: this._headers,
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
+    }).then(this._checkResponse);
   }
 }
 const api = new Api({
